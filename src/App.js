4d45c34de22532a9bb588402c1907717153926d9
burneverything1 <63659097+burneverything1@users.noreply.github.com>
2021-09-17 18:08:53 -0400

    togglable component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import PlantForm from './components/PlantForm'
+import Togglable from './components/Togglable'
 
 import plantService from './services/plants'
 
 const App = () => {
 
   const createPlant = async (plantObject) => {
+    plantFormRef.current.toggleVisibility()
     try {
       const savedPlant = await plantService.create(plantObject)
       // do something with plant
     } catch (exception){
       console.log(exception);
     }
-  } 
+  }
+
+  const plantFormRef = useRef()
 
   return (
     <div>
       <h1>LettuceSwap</h1>
-      <PlantForm createPlant={createPlant}/>
+      <Togglable buttonLabel='New Plant' ref={plantFormRef}>
+        <PlantForm createPlant={createPlant}/>
+      </Togglable>
     </div>
   )
 }

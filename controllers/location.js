b6d9ac1c5856f7a8cb083b162429283d7f8a98c7
burneverything1 <63659097+burneverything1@users.noreply.github.com>
2021-09-18 16:15:29 -0400

    location display working

--- controllers/location.js
+++ controllers/location.js
@@ -1,6 +1,22 @@
 const locationRouter = require('express').Router()
 const LocationData = require('../models/location.js')
 
+locationRouter.get('/', async (request, response) => {
+    const locations = await LocationData.find({})
+
+    const payload = {}
+
+    locations.forEach(location => {
+        let loc_string = `${location.city}, ${location.state}`
+        if (loc_string in payload) {
+            payload[loc_string] += 1
+        } else {
+            payload[loc_string] = 1
+        }
+    })
+    response.json(payload)
+})
+
 locationRouter.post('/', async (request, response, next) => {
     const body = request.body
 

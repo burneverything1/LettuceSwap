e642b9f7a76a88ea724be69a7143b841a0746844
burneverything1 <63659097+burneverything1@users.noreply.github.com>
2021-09-17 21:37:26 -0400

    chart display

--- src/components/PlantPriceDisplay.js
+++ src/components/PlantPriceDisplay.js
@@ -1,6 +1,33 @@
 import React from 'react'
+import { Bar } from 'react-chartjs-2'
 
 const PlantPriceDisplay = ({ priceData }) => {
+
+    const bid_labels = []
+    const bid_data = []
+    for (const price in priceData.bids) {
+        bid_labels.push(price)
+        bid_data.push(priceData.bids[price])
+    }
+
+    const ask_lables = []
+    const ask_data = []
+    for (const price in priceData.bids) {
+        ask_lables.push(price)
+        ask_data.push(priceData.asks[price])
+    }
+
+
+    const bid_chart = {
+        labels: bid_labels,
+        datasets: [{
+            label: 'Bid Prices',
+            data: bid_data
+        }, {
+            label: 'Ask Prices',
+            data: ask_data
+        }]
+    }
 
     const createDisplay = (type, priceData) => {
         // must return an array to display properly
@@ -13,8 +40,7 @@ const PlantPriceDisplay = ({ priceData }) => {
 
     return (
         <div>
-            Bids: {createDisplay('bids', priceData.bids)}
-            Asks: {createDisplay('asks', priceData.asks)}
+            <Bar data={bid_chart}/>
         </div>
     )
 }
